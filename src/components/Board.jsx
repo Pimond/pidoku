ef400691f6ffd81bf3cdf716ded9f71b114a8ff9
Pimond <cyspho@Outlook.com>
2025-07-20 21:51:40 +0330

    Fixed square on mobile

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -33,8 +33,8 @@ export default function Board({ board, selected, onCellSelect }) {
   }
 
   return (
-    <div className="inline-block bg-gray-200 p-2 rounded-md shadow-lg select-none">
-      <div className="grid grid-cols-9">
+    <div className="inline-block bg-gray-200 rounded-md shadow-lg select-none">
+      <div className="grid grid-cols-9 w-full aspect-square">
         {board.map((row, rowIdx) =>
           row.map((cell, colIdx) => (
             <Cell
